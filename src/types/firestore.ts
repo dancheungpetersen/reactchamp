f34f3c91dc6843d41f8b20cec087586269051259
Dan Cheung Petersen <dan.cheung.42@gmail.com>
2025-10-16 18:36:12 +0800

    Added security rule and comments

--- src/types/firestore.ts
+++ src/types/firestore.ts
@@ -7,50 +7,57 @@ export interface ScoringRules {
   bonusForExactScore: number;
 }
 
+/**
+ * All fields are editable by pool owners, except id
+ */
 export interface Pool {
   id: string;
-  name: string;
+  name: string; 
   description: string;
-  logo?: string;
+  logo?: string; 
   timezone: string;
   betLockTime: Timestamp;
   scoringRules: ScoringRules;
+  ownerIds: string[]; // Array of Firebase Auth UIDs
 }
 
 /**
  * Represents a document in the 'Participants' subcollection under a 'Pool' document.
  */
 export interface Participant {
-  id: string; // Corresponds to Firebase Auth UID
-  displayName: string;
-  isAdmin: boolean;
-  hasPaid: boolean;
-  timezone: string;
-  totalPoints?: number;
+  UserId: string; // Corresponds to Firebase Auth UID, not editable
+  displayName: string; // editable by user with UserId
+  isAdmin: boolean; // editable by owners
+  hasPaid: boolean; // editable by owners
+  timezone: string; // editable by user with UserId
+  totalPoints?: number; // not editable, calculated field
 }
 
 /**
  * Represents a document in the 'Teams' subcollection under a 'Pool' document.
- */
+ * only editable by pool owners  
+*/
 export interface Team {
-  id: string;
+  id: string; 
   name: string;
   iconUrl: string;
 }
 
 /**
  * Represents a document in the 'Groups' subcollection under a 'Pool' document.
- */
+ * only editable by pool owners 
+*/
 export interface Group {
   id: string;
   name: string; // e.g., "Group A"
-  teamIds: string[];
+  teamIds: string[]; 
 }
 
 export type MatchStatus = "scheduled" | "in-progress" | "finished" | "scored";
 
 /**
  * Represents a document in the 'Matches' subcollection under a 'Pool' document.
+ * Only editable by pool owners
  */
 export interface Match {
   id: string;
@@ -67,11 +74,13 @@ export interface Match {
 
 /**
  * Represents a document in the 'Bets' subcollection under a 'Match' document.
+ * Fields cannot be updated after betLockedTime
  */
 export interface Bet {
-  participantId: string;
-  matchId: string;
-  homeGoalsBet: number;
+  participantId: string; // set when bet is created, not editable
+  matchId: string; // set when bet is created, not editable
+  homeGoalsBet: number; 
   awayGoalsBet: number;
-  pointsAwarded?: number;
+  betLockedTime: Timestamp; // set when bet is created, not editable
+  pointsAwarded?: number; // calculated after match is finished
 }
